7139a87adf5162cabde0fce53e996a0e27384224
Alberto Moreno <alberto.moreno.sa@gmail.com>
2016-07-12 01:06:02 -0500

    BT04 Creating Our First Landscape

---
+++ BatttleTank/Source/BatttleTank/BatttleTankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BatttleTankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTTLETANK_API ABatttleTankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
